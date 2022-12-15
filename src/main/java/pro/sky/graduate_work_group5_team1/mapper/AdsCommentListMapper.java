d25f379b9ca33e72307c1826fd956755ae65ade6
Alexey214 <golenev214@gmail.com>
2022-12-15 18:32:55 +0300

    добавил в аннотацию мапперов, работающих со списками: injectionStrategy = InjectionStrategy.CONSTRUCTOR

--- src/main/java/pro/sky/graduate_work_group5_team1/mapper/AdsCommentListMapper.java
+++ src/main/java/pro/sky/graduate_work_group5_team1/mapper/AdsCommentListMapper.java
@@ -1,12 +1,13 @@
 package pro.sky.graduate_work_group5_team1.mapper;
 
+import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import pro.sky.graduate_work_group5_team1.model.AdsComment;
 import pro.sky.graduate_work_group5_team1.model.dto.AdsCommentDto;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring", uses = AdsCommentMapper.class)
+@Mapper(componentModel = "spring", uses = AdsCommentMapper.class, injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface AdsCommentListMapper {
 
     List<AdsCommentDto> toDtoList(List<AdsComment> adsCommentList);
--- src/main/java/pro/sky/graduate_work_group5_team1/mapper/AdsListMapper.java
+++ src/main/java/pro/sky/graduate_work_group5_team1/mapper/AdsListMapper.java
@@ -1,12 +1,13 @@
 package pro.sky.graduate_work_group5_team1.mapper;
 
+import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import pro.sky.graduate_work_group5_team1.model.Ads;
 import pro.sky.graduate_work_group5_team1.model.dto.AdsDto;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring", uses = AdsMapper.class)
+@Mapper(componentModel = "spring", uses = AdsMapper.class, injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface AdsListMapper {
 
     List<Ads> toModelList(List<AdsDto> adsDtoList);
