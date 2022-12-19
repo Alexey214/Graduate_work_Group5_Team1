760a0c1e9312e37d1e4d5af8c8783fd1264f075a
Alexey214 <golenev214@gmail.com>
2022-12-19 16:36:11 +0300

    описал JavaDoc для методов UserService.java

--- src/main/java/pro/sky/graduate_work_group5_team1/service/UserService.java
+++ src/main/java/pro/sky/graduate_work_group5_team1/service/UserService.java
@@ -1,8 +1,11 @@
 package pro.sky.graduate_work_group5_team1.service;
 
+import pro.sky.graduate_work_group5_team1.exeption.UserNotFoundException;
 import pro.sky.graduate_work_group5_team1.model.dto.NewPassword;
 import pro.sky.graduate_work_group5_team1.model.dto.ResponseWrapperUser;
 import pro.sky.graduate_work_group5_team1.model.dto.UserDto;
+
+import java.util.List;
 
 public interface UserService {
 
@@ -10,14 +13,31 @@ public interface UserService {
      * Метод поиска пользователя по Id
      *
      * @param id персональный идентификатор пользователя
-     * @return пользователя с данным идентификатором
+     * @return пользователя с данным идентификатором, либо {@link UserNotFoundException}
      */
     UserDto getUser(Integer id);
 
+    /**
+     * Метод поиска всех пользователей
+     *
+     * @return экземпляр {@link ResponseWrapperUser}, внутри которого содержится {@link List<UserDto>}
+     */
     ResponseWrapperUser getUsers();
 
+    /**
+     * Метод для замены пароля пользователя
+     *
+     * @param newPassword сущность, содержащая текущий и новый пароль пользователя
+     * @return экземпляр {@link NewPassword}
+     */
     NewPassword setPassword(NewPassword newPassword);
 
+    /**
+     * Метод для изменения данных
+     *
+     * @param userDto сущность, содержащая изменённые данные пользователя
+     * @return экземпляр {@link UserDto}
+     */
     UserDto updateUser(UserDto userDto);
 
 }
