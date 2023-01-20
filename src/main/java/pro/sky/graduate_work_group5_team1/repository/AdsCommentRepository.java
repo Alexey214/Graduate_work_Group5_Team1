fd18beb632b9cfa724d621c0fdac424d28cf3d89
Alexey214 <golenev214@gmail.com>
2023-01-20 22:08:30 +0300

    произвёл рефакторинг сервиса AdsServiceImpl.java + снёс класс с cors фильтром, заменив его бином в конфиге (пакет секьюрити)

--- src/main/java/pro/sky/graduate_work_group5_team1/repository/AdsCommentRepository.java
+++ src/main/java/pro/sky/graduate_work_group5_team1/repository/AdsCommentRepository.java
@@ -12,10 +12,7 @@ import java.util.Optional;
 @Repository
 public interface AdsCommentRepository extends JpaRepository<AdsComment, Integer> {
 
-    //    @Query(value = "select ac.* from ads_comment as ac " +
-//            "inner join ads on ads.id = 1? " +
-//            "inner join users u on u.id = 2?", nativeQuery = true)
-    @Query("select e from AdsComment e where e.id = :id and e.pk.pk = :pk")
+    @Query(value = "select ac from AdsComment as ac where ac.id = :id and ac.pk.pk = :pk")
     Optional<AdsComment> findAdsCommentByPkAndId(Integer pk, Integer id);
 
     @Query("select e from AdsComment e where e.pk.pk = :pk")
